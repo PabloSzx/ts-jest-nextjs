73d76baa7eb609435103267a2c78fdf453d73434
PabloSz <pablosaez1995@gmail.com>
2020-01-01 21:53:13 -0300

    .babelrc

--- jest.config.js
+++ jest.config.js
@@ -5,29 +5,30 @@ module.exports = {
   globals: {
     "ts-jest": {
       tsConfig: "__tests__/tsconfig.json",
-      babelConfig: {
-        comments: false
-        // plugins: ["@babel/plugin-transform-for-of"]
-        // plugins: [
-        //   [
-        //     require("./plugins/jsx-pragma"),
-        //     {
-        //       // This produces the following injected import for modules containing JSX:
-        //       //   import React from 'react';
-        //       //   var __jsx = React.createElement;
-        //       module: "react",
-        //       importAs: "React",
-        //       pragma: "__jsx",
-        //       property: "createElement"
-        //     }
-        //   ]
-        // ]
-      }
-
-      // {
-      //   presets: ["next/babel"]
-      //   // plugins: [require("@babel/plugin-syntax-dynamic-import")]
+      // babelConfig: {
+      //   comments: false
+      //   // plugins: ["@babel/plugin-transform-for-of"]
+      //   // plugins: [
+      //   //   [
+      //   //     require("./plugins/jsx-pragma"),
+      //   //     {
+      //   //       // This produces the following injected import for modules containing JSX:
+      //   //       //   import React from 'react';
+      //   //       //   var __jsx = React.createElement;
+      //   //       module: "react",
+      //   //       importAs: "React",
+      //   //       pragma: "__jsx",
+      //   //       property: "createElement"
+      //   //     }
+      //   //   ]
+      //   // ]
       // }
+
+      // babelConfig: "<rootDir>/.babelrc"
+
+      babelConfig: {
+        presets: ["next/babel"]
+      }
     }
   }
 };
