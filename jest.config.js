efe0e372890a2d02932b67df7804ac5f6b793e5c
PabloSz <pablosaez1995@gmail.com>
2020-01-01 21:47:13 -0300

    babel config problems

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,33 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  // preset: "@pablosz/ts-jest",
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
   globals: {
     "ts-jest": {
       tsConfig: "__tests__/tsconfig.json",
-    },
-  },
+      babelConfig: {
+        comments: false
+        // plugins: ["@babel/plugin-transform-for-of"],
+        // plugins: [
+        //   [
+        //     require("./plugins/jsx-pragma"),
+        //     {
+        //       // This produces the following injected import for modules containing JSX:
+        //       //   import React from 'react';
+        //       //   var __jsx = React.createElement;
+        //       module: "react",
+        //       importAs: "React",
+        //       pragma: "__jsx",
+        //       property: "createElement"
+        //     }
+        //   ]
+        // ]
+      }
+
+      // {
+      //   presets: ["next/babel"]
+      //   // plugins: [require("@babel/plugin-syntax-dynamic-import")]
+      // }
+    }
+  }
 };
